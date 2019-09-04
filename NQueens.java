ef7aba512d05a3f9d743f04e574a285b3fd51e60
chia <aesparzachavez@dons.usfca.edu>
2019-09-04 13:03:25 -0700

    100%

--- NQueens.java
+++ NQueens.java
@@ -1,11 +1,11 @@
 public class NQueens {
 
    private int N;
-   private int [][]board;
+   private int [][] board;
 
     NQueens(int n){
         N = n;
-        board = new int[N][N];
+        board = new int [N][N];
     }
 
     public boolean placeNQueens() throws Exception {
@@ -13,59 +13,78 @@ public class NQueens {
             throw new Exception();
         }
 
-        return placeQ(0);
-    }
-
-    public boolean isAttacked(int row, int col){
-
-        //check row
-        for (int i = 0; i < col; i++){
-            if(board[row][i] == 1){
-                return false;
-            }
+        if (placeQ(0)){
+            printToConsole();
+            return true;
+        } else {
+            return false;
         }
-
-        //check left to right diagonal - down
-        for (int i = row, j = col; i < N && j >= 0; i++, j--){
-            if(board[i][j] == 1){
-                return false;
-            }
-        }
-
-        //check left to right diagonal - up
-        for (int i = row, j = col; i < N  && j >= 0; i--, j--){
-            if(board[i][j] == 1){
-                return false;
-            }
-        }
-        return true;
     }
 
     public boolean placeQ(int row){
         if(row == N)
             return true;
 
-        //iterate through whole board
+        //iterate through whole board until space is not attacked
         for (int col = 0; col < N; col++){
-            if (isAttacked(row, col)) {
+            if (!isAttacked(row, col)) {
                 board[row][col] = 1;
-                if (placeQ(row + 1))
+                if (placeQ(row + 1))  //recursion
                     return true;
-                board[row][col] = 0; //backtrack
+                board[row][col] = 0; //backtrack: undoes that placeQ
             }
         }
+        return false;
+    }
+
+    public boolean isAttacked(int row, int col){
+
+        //check col
+        for (int i = 0; i < N; i++){
+            if(board[i][col] == 1){
+                return true;
+            }
+        }
+
+        //check left diagonal - down
+        for (int i = row, j = col; i < N && j >= 0; i++, j--){
+            if(board[i][j] == 1){
+                return true;
+            }
+        }
+
+        //check left diagonal - up
+        for (int i = row, j = col; i >= 0  && j >= 0; i--, j--){
+            if(board[i][j] == 1){
+                return true;
+            }
+        }
+
+        //check right diagonal = down
+        for (int i = row, j = col; i >= 0 && j < N; i--, j++){
+            if(board[i][j] == 1){
+                return true;
+            }
+        }
+
+        //check right diagonal - up
+        for (int i = row, j = col; i < N && j < N; i++, j++) {
+            if (board[i][j]==1)
+                return true;
+        }
+
         return false;
     }
 
     public void printToConsole() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (board[i][j] == 0)
-                    System.out.print("___ ");
-                else
+                if (board[i][j] == 1)
                     System.out.print("Q ");
+                else
+                    System.out.print("_ ");
             }
+            System.out.println();
         }
-        System.out.println();
     }
 }
